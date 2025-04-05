b66621c4dbd56b92d809e5c76968d07014d634f9
Kramarenko Denys <krama4d@gmail.com>
2025-04-05 17:42:15 +0200

    Update to run in AWS

--- ocrmypdf-aws-terraform/providers.tf
+++ ocrmypdf-aws-terraform/providers.tf
@@ -4,39 +4,42 @@
 
 provider "aws" {
   region = var.region
-  access_key                  = "test"
-  secret_key                  = "test"
-  skip_credentials_validation = true
-  skip_requesting_account_id  = true
-  skip_metadata_api_check     = true
-  s3_use_path_style           = true
-endpoints {
-  apigateway     = "http://localhost:4566"
-  appautoscaling = "http://localhost:4566"
-  cloudformation = "http://localhost:4566"
-  cloudwatch     = "http://localhost:4566"
-  cloudwatchlogs = "http://localhost:4566"
-  dynamodb       = "http://localhost:4566"
-  ec2            = "http://localhost:4566"
-  ecr            = "http://localhost:4566"
-  ecs            = "http://localhost:4566"
-  es             = "http://localhost:4566"
-  firehose       = "http://localhost:4566"
-  iam            = "http://localhost:4566"
-  kinesis        = "http://localhost:4566"
-  kms            = "http://localhost:4566"
-  lambda         = "http://localhost:4566"
-  redshift       = "http://localhost:4566"
-  route53        = "http://localhost:4566"
-  s3             = "http://localhost:4566"
-  secretsmanager = "http://localhost:4566"
-  ses            = "http://localhost:4566"
-  sns            = "http://localhost:4566"
-  sqs            = "http://localhost:4566"
-  ssm            = "http://localhost:4566"
-  stepfunctions  = "http://localhost:4566"
-  sts            = "http://localhost:4566"
-}
+#
+# Uncoment if you want to use localstack
+#
+#   access_key                  = "test"
+#   secret_key                  = "test"
+#   skip_credentials_validation = true
+#   skip_requesting_account_id  = true
+#   skip_metadata_api_check     = true
+#   s3_use_path_style           = true
+# endpoints {
+#   apigateway     = "http://localhost:4566"
+#   appautoscaling = "http://localhost:4566"
+#   cloudformation = "http://localhost:4566"
+#   cloudwatch     = "http://localhost:4566"
+#   cloudwatchlogs = "http://localhost:4566"
+#   dynamodb       = "http://localhost:4566"
+#   ec2            = "http://localhost:4566"
+#   ecr            = "http://localhost:4566"
+#   ecs            = "http://localhost:4566"
+#   es             = "http://localhost:4566"
+#   firehose       = "http://localhost:4566"
+#   iam            = "http://localhost:4566"
+#   kinesis        = "http://localhost:4566"
+#   kms            = "http://localhost:4566"
+#   lambda         = "http://localhost:4566"
+#   redshift       = "http://localhost:4566"
+#   route53        = "http://localhost:4566"
+#   s3             = "http://localhost:4566"
+#   secretsmanager = "http://localhost:4566"
+#   ses            = "http://localhost:4566"
+#   sns            = "http://localhost:4566"
+#   sqs            = "http://localhost:4566"
+#   ssm            = "http://localhost:4566"
+#   stepfunctions  = "http://localhost:4566"
+#   sts            = "http://localhost:4566"
+# }
   default_tags {
     tags = {
       Project     = "OCRMyPDF"  # Project tag
@@ -56,7 +59,7 @@ provider "docker" {
 }
 
 # Data source for AWS ECR authorization token
-#data "aws_ecr_authorization_token" "token" {}
+data "aws_ecr_authorization_token" "token" {}
 
 # Terraform settings and backend configuration for remote state storage
 terraform {
@@ -70,14 +73,13 @@ terraform {
       version = "~> 3.0"
     }
   }
-}
 #  ╔╦╗╔═╗╦═╗╦═╗╔═╗╔═╗╔═╗╦═╗╔╦╗  ╔═╗╔╦╗╔═╗╔╦╗╔═╗
 #   ║ ║╣ ╠╦╝╠╦╝╠═╣╠╣ ║ ║╠╦╝║║║  ╚═╗ ║ ╠═╣ ║ ║╣ 
 #   ╩ ╚═╝╩╚═╩╚═╩ ╩╚  ╚═╝╩╚═╩ ╩  ╚═╝ ╩ ╩ ╩ ╩ ╚═╝
 
-#   backend "s3" {
-#     bucket = "ocrmypdf-state-bucket"
-#     key    = "ocrmypdf/terraform.tfstate"
-#     region = var.region
-#   }
-# }
+  backend "s3" {
+    bucket = "ocrmypdf-state-bucket"
+    key    = "ocrmypdf/terraform.tfstate"
+    region = var.region
+  }
+}
